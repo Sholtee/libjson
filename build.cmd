d6b14b4516a5df113401be3739781c3ee1ac4b8c
Dénes Solti <sodnaatx@gmail.com>
2019-05-15 09:14:56 +0200

    build V

--- build.cmd
+++ build.cmd
@@ -7,11 +7,14 @@
 
 @echo installing Lazarus:
 call build\install-lazarus
+if %errorlevel% neq 0 exit /b %errorlevel%
 echo:
 
 @echo starting build:
+setlocal enabledelayedexpansion
 for /F %%F in ('dir /s /b src\*.lpr tests\*.lpr') do (
   @echo: 
   @echo build file: %%F 
   call build\bin\lazbuild.exe -B %%F
+  if !errorlevel! neq 0 exit /b !errorlevel!
 )
--- build/install-lazarus.cmd
+++ build/install-lazarus.cmd
@@ -10,6 +10,7 @@ set bin=%~dp0bin
 
 @echo extracting files...
 powershell.exe -nologo -noprofile -command "& { Add-Type -A  'System.IO', 'System.IO.Compression.FileSystem'; if (![IO.Directory]::Exists('%bin%')) {[IO.Compression.ZipFile]::ExtractToDirectory('%zip%', '%bin%');} }"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 @echo update config...
 @echo --primary-config-path=%bin%>%bin%\lazarus.cfg
@@ -18,6 +19,7 @@ set fpc=%bin%\fpc\3.0.4
 set fpcbin=%fpc%\bin\i386-win32
 
 call "%fpcbin%\fpcmkcfg.exe" -d basepath="%fpc%" -o "%fpcbin%\fpc.cfg"
+if %errorlevel% neq 0 exit /b %errorlevel%
 
 set xml=%bin%\environmentoptions.xml
 
